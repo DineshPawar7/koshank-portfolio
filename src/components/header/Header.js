95646a3e107ada1de7bee56ae017907adaa1f15e
Dinesh Pawar <dsp70458@gmail.com>
2025-02-18 00:05:07 +0530

    update header.css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,22 +3,21 @@ import './Header.css';
 import Logo from '../../assets/koshank-logo.png';
 import { TiThMenu } from "react-icons/ti";
 
-
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <header className="header">
       <div className="header-logo">
-        <img src={Logo} alt="header-Logo"/>
+        <img src={Logo} alt="header-Logo" />
       </div>
 
-    {/* The button triggers the menu to open/close in mobile and responsive views based on the current state (menuOpen). */}
+      {/* Toggle the menu open/close */}
       <button className="header-hamburger" onClick={() => setMenuOpen(!menuOpen)}>
         <TiThMenu />
       </button>
 
-      {/* Conditionally applies the 'header-active' class to the navigation menu when the menu is open (menuOpen is true). */}
+      {/* Conditional class based on menuOpen */}
       <nav className={`header-nav ${menuOpen ? 'header-active' : ''}`}>
         <ul className="header-nav-list">
           <li><a href="#process">Process</a></li>
