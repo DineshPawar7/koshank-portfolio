c9b952927b9245846c8df0866166b62a21d35926
Dinesh Pawar <dsp70458@gmail.com>
2025-02-22 12:07:37 +0530

    update website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import './App.css';
- import Benefits from './components/benefits/Benefits';
- import Capabilities from './components/capabilities/Capabilities';
+import Benefits from './components/benefits/Benefits';
+import Capabilities from './components/capabilities/Capabilities';
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero-Section';
- import HowWeWork from './components/howWeWork/HowWeWork'; 
- import RecentProjects from './components/recentprojects/RecentProjects';
- import Testimonial from './components/testimonial/Testimonial';
- import TrustedBrand from './components/trustedBrands/TrustedBrands';
- import PricingSection from './components/pricingSection/PricingSection';
-//  import BlogSection from './components/blogSection/BlogSection';
-//  import FAQs from './components/faqs/FAQs';
- import Footer from './components/footer/Footer';
- import OtherProject from './components/recentprojects/OtherProject';
+import HowWeWork from './components/howWeWork/HowWeWork'; 
+import RecentProjects from './components/recentprojects/RecentProjects';
+import Testimonial from './components/testimonial/Testimonial';
+import TrustedBrand from './components/trustedBrands/TrustedBrands';
+import PricingSection from './components/pricingSection/PricingSection';
+// import BlogSection from './components/blogSection/BlogSection';
+// import FAQs from './components/faqs/FAQs';
+import Footer from './components/footer/Footer';
+import OtherProject from './components/recentprojects/OtherProject';
+import Whatsapp from './components/wa-page/Whatsapp'; // Import the WhatsApp Icon component
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
-       <TrustedBrand />
+      <TrustedBrand />
       <HowWeWork />
       <RecentProjects />
       <OtherProject />
@@ -27,11 +28,10 @@ function App() {
       <Benefits />
       <Testimonial />
       <PricingSection />
-      {/* <BlogSection />
-      <FAQs /> */}
+      {/* <BlogSection /> */}
+      {/* <FAQs /> */}
       <Footer />
-
-      
+      <Whatsapp /> {/* Add the WhatsApp Icon component */}
     </div>
   );
 }
---
+++ src/components/wa-page/Whatsapp.js
@@ -0,0 +1,22 @@
+// src/components/WhatsAppIcon.js
+
+import React from 'react';
+import { FaWhatsappSquare } from 'react-icons/fa';
+import './Whatsapp.css';
+
+const WhatsAppIcon = () => {
+  const phoneNumber = '+919209231192';
+  const message = 'Hello, I need assistance!';
+
+  const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
+  return (
+    <div className="whatsapp-icon">
+      <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
+        <FaWhatsappSquare className="whatsapp-icon__icon" />
+      </a>
+    </div>
+  );
+};
+
+export default WhatsAppIcon;
